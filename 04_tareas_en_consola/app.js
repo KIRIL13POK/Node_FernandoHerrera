cfaa9c635d6bbd06ab3949bd7b0487a4b8cfaaab
Kiril Pokrovskiy <p1kr1vskiy@gmail.com>
2023-05-05 15:35:51 +0200

    stdin - stdout - Readline
    Crear interfaz en la consola

--- 04_tareas_en_consola/app.js
+++ 04_tareas_en_consola/app.js
@@ -1,9 +1,19 @@
 require('colors');
+
+
+const { mostrarMenu, pausa } = require('./helpers/mensales');
+
+
+
+
 console.clear()
 
 
 const main = async() => {
     console.log('Hola Mundo')
+
+    mostrarMenu();
+    pausa();
 
 
 
---
+++ 04_tareas_en_consola/helpers/mensales.js
@@ -0,0 +1,49 @@
+require('colors');
+
+const mostrarMenu = () => {
+    console.clear()
+    console.log('===================================='.green)
+    console.log('      Seleccione una opcion'.red)
+    console.log('====================================\n'.green)
+
+
+    console.log(`${'1'.red}. Crear una tarea`)
+    console.log(`${'2'.red}. Listar tareas`)
+    console.log(`${'3'.red}. Listar tareas complitadas`)
+    console.log(`${'4'.red}. Listar tareas pendientes`)
+    console.log(`${'5'.red}. Completar tarea(s)`)
+    console.log(`${'6'.red}. Borrar tarea`)
+    console.log(`${'0'.red}. Salir\n`)
+    // => Creando interfaz:
+    const readLine = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    // => Una manera de  recibir la informacion:
+    readLine.question('Seleccione una opcion', (opt) => {
+        //console.log({opt}); para ver la informacion
+        readLine.close();
+
+    })
+};
+
+const pausa = () => {
+    const readLine = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    
+    readLine.question(`\nPresione ${'Enter'.red} para continuar\n`, (opt) => {
+        readLine.close();
+
+    })
+
+}
+
+module.exports = {
+    mostrarMenu,
+    pausa
+    
+}
+
+
