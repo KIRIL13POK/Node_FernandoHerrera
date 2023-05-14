23a7d085a57a12a6e2a03a750423ed169a661587
Kiril Pokrovskiy <p1kr1vskiy@gmail.com>
2023-05-15 00:04:43 +0200

    Transformar objeto a un arreglo - Detalles estéticos

--- 04_tareas_en_consola/app.js
+++ 04_tareas_en_consola/app.js
@@ -6,14 +6,9 @@ const { inquirerMenu,
   pausa,
   leerInput
 } = require('./helpers/inquirer');
+
 const TareaS = require('./models/TareaS');
 //const { mostrarMenu, pausa } = require('./helpers/mensales');
-
-
-
-
-console.clear()
-
 
 const main = async () => {
 
@@ -21,19 +16,17 @@ const main = async () => {
   const tareaS = new TareaS()
 
   do {
+    //Imprimir el menu
     opt = await inquirerMenu();
     switch (opt) {
       case '1':
         //crear opcion
         const descDeLaTarea = await leerInput('Descripcion: ')
         tareaS.crearTarea(descDeLaTarea);
-
-
         break;
       case '2':
         console.log(tareaS._listado)
-
-
+        console.log(tareaS.listadoArr)
         break;
 
 
