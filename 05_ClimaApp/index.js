e9c256edae931a78458e13748af1614642374e5e
Kiril Pokrovskiy <p1kr1vskiy@gmail.com>
2023-06-06 17:15:25 +0200

    Modelo para controlar la aplicación

--- 05_ClimaApp/index.js
+++ 05_ClimaApp/index.js
@@ -1,17 +1,44 @@
-const { leerInput, inquirerMenu,pausa } = require('./helpers/inquirer')
+const { leerInput, inquirerMenu, pausa } = require('./helpers/inquirer');
+const Busquedas = require('./models/busquedas');
 
 
 const main = async () => {
 
+    const busquedas = new Busquedas();
     let opt;
+
 
     do {
         //Imprimir el menu
         opt = await inquirerMenu();
-        console.log({opt})
-        
-        
-        if(opt !== 0) await pausa();
+        switch (opt) {
+            case 1:
+                //Mostrar mensaje
+                const lugar = await leerInput('Ciudad:');
+                console.log(lugar)
+
+                //Buscar los lugares
+
+                //Seleccionar el lugar
+
+                //Clima
+
+                //Mostrar resultados
+                console.log('\nInformación de la ciudad\n'.green);
+                console.log('Ciudad:')
+                console.log('Lat:')
+                console.log('Lng:')
+                console.log('Temperatura:')
+                console.log('Minima:')
+                console.log('Maxima:')
+                
+                break;
+
+
+        }
+
+
+        if (opt !== 0) await pausa();
 
     } while (opt !== 0);
 
---
+++ 05_ClimaApp/models/busquedas.js
@@ -0,0 +1,22 @@
+class Busquedas  {
+
+    historial = ['Madrid', 'Santiago','Valencia', 'Sevilla'];
+    constructor(){
+        //TODO: Leer DB si existe
+
+    }
+
+    //Método para buscar un lugar
+    async ciudad(lugar = ''){
+        //Peticion http
+        console.log(lugar)
+
+        return [] ; //Retornar los lugares
+
+    }
+
+}
+
+
+
+module.exports = Busquedas;
